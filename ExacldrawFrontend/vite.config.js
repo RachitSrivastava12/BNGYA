301e08d93689942b462bfaea67ea6a5db1add2a1
Rachit Srivastava <racitsrivastava76@gmail.com>
2024-06-25 17:50:50 +0530

    Update frontend and connect to backend

--- ExacldrawFrontend/vite.config.js
+++ ExacldrawFrontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://your-backend-url.example',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
